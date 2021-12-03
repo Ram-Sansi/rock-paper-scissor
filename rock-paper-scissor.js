2888fcedc09bf0b7861cc76f11c16e4f30996208
Ram Sansi <singhramdas180@gmail.com>
2021-12-03 17:26:13 +0530

    create rock-paper-scissor repository
    created rock paper scissor repository and added computerPlay function that returns random string from the given array

---
+++ rock-paper-scissor.js
@@ -0,0 +1,15 @@
+// rock-paper-scissor project
+
+console.log("This is a Rock paper scissor game");
+
+const computerArray = ["Rock", "Paper", "Scissor"];
+
+function computerPlay(){
+    return computerArray[Math.floor(Math.random() * myArray.length)];
+    
+}
+
+
+function playRound(playerSelection, computerSelection){
+
+}
