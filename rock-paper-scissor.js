43fe415c16f28dfd97d293700b3bd1f1beb99224
Ram Sansi <singhramdas180@gmail.com>
2021-12-10 18:58:37 +0530

    fix game function
    fixed game function. The code wasn't running properly on chrome but worked well on firefox and codepen. don't know what the issue with chrome is/was. Taken help from odin project community and figured out the error

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,5 +1,4 @@
-// rock-paper-scissor project
-
+// rock paper scissors project
 console.log("This is a Rock paper scissor game");
 
 const computerArray = ["rock", "paper", "scissors"];
@@ -9,60 +8,72 @@ function computerPlay(){
     
 }
 
-
-
-
 function playRound(playerSelection, computerSelection) {
   playerSelection = window.prompt( "Select either Rock, Paper, or Scissors" ).toLowerCase();
-  computerSelection = computerPlay()
-  console.log(playerSelection)
-  console.log(computerSelection)
-  computerScore = 0;
-  let playerScore = 0;
-
-  if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    playerScore+=1;
-    return (`You win this round! Rock beats Scissors. Your score ${playerScore} Let's try again!`);
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    playerScore+=1;
-    console.log(`You win this round! Paper beats Rock. Your score is ${playerScore} Let's try again!`);
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    playerScore+=1;
-    console.log(`You win this round! Scissors beats Paper. Your score is ${playerScore} Let's try again!`);
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    computerScore+1;
-    console.log(`You lose this round, paper beats Rock. Your score is ${computerScore} Let's try again!`);
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    computerScore+=1;
-    console.log(`You lose this round. Scissors beats Paper. Your score is ${computerScore} Let's try again!`);
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    computerScore+=1;
-      console.log(`You lose this round! Rock beats Scissors. Your score is ${computerScore} Let's try again!`);
-  } else { 
-    console.log(`It's a tie!`);
-  }
-
-}
+  
+  computerSelection = computerPlay();
+  console.log(playerSelection);
+  console.log(computerSelection);
+  
+  
+   if (playerSelection === 'rock' && computerSelection === 'scissors') {
     
+    
+    return("You win this round! Rock beats scissors. Let's try again!");
+    
+   
+    
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
 
+    return("You win this round! Paper beats Rock. Let's try again!");
+
+  }
+  
+   else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+   
+    return ("You win this round! Scissors beats Paper. Let's try again!");
+
+  } 
+  
+  else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    
+   
+    return("You lose this round, paper beats Rock. Let's try again!");
+
+  } 
+  
+  else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+
+    
+    return ("You lose this round. Scissors beats Paper.Let's try again!");
+
+  } 
+  else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+
+    return  ("You lose this round! Rock beats Scissors.Let's try again!");
+
+  } else { 
+  
+    return("It's a tie!" +  computerSelection);
+    
+  }
+  
+}
 
 function game(){
-  for (let i=1; i<=5; i++)
-  playRound(i);
-  
-  
-    // console.log(`Your score is ${playerScore}.`);
-  // return computerScore;
-  
-  
+  for (round=1; round<=5; round++){
+     
+   selectionGame = playRound();
+   console.log(selectionGame)
+    
+    
+    
+  }
   
 }
 
-// if the computer wins, +1 to its score. if the player wins, add +1 to the score. 
-// compare score of both computer and player. If playerScore > computerScore, player wins
-// else computer wins or player lose
 
 
-// declare a variable at each possible match
-// if its a win add +1 and if its a lose +0 or just do nothing
-// at the loop add all the variable values together and return the results
+
+
+
