61640ddae0cfb6e8fdb39c69135a1aa1103f1ff7
Ram Sansi <singhramdas180@gmail.com>
2021-12-10 21:29:36 +0530

    add score function
    added score function inside the game function and call it outside when the game function executes itself 5 times.The score function shows the overall winner and the score of the player

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,7 +1,9 @@
 // rock paper scissors project
 console.log("This is a Rock paper scissor game");
-
+  
 const computerArray = ["rock", "paper", "scissors"];
+  let computerScore = 0;
+  let playerScore = 0;
 
 function computerPlay(){
     return computerArray[Math.floor(Math.random() * computerArray.length)];
@@ -18,12 +20,16 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'rock' && computerSelection === 'scissors'||
       playerSelection === 'paper' && computerSelection === 'rock'||
       playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore+=1;
+        console.log("Your Score:"+playerScore);
       return(`You win this round! ${playerSelection} beats ${computerSelection} Let's try again!`);
 }
 
   else if(playerSelection === 'rock' && computerSelection === 'paper'||
           playerSelection === 'paper' && computerSelection === 'scissors'||
           playerSelection === 'scissors' && computerSelection === 'rock') {
+            computerScore+=1;
+            console.log("Computer Score:"+computerScore);
           return (`You lose this round! ${computerSelection} beats ${playerSelection} Let's try again!`);
 }
 
@@ -37,10 +43,22 @@ function game(){
   for (round=1; round<=5; round++){
      
    selectionGame = playRound();
-   console.log(selectionGame)
+   console.log(selectionGame);
+   function score(){
+   if(playerScore > computerScore){
+     console.log("You won the game! Your score is: " +playerScore);
+   }
+   else if(playerScore === computerScore) {
+     console.log("It's a tie!");
+   }
+   else{
+     console.log("You lost the game!");
+   }
     
    }
   
+  }
+  overAllResult = score();
 }
 
 
